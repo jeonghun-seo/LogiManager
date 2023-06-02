ae405e7d77200e138f267a912b5fa1a9e02c201f
JeongHun <JeongHun.Seo@outlook.kr>
2023-06-02 20:53:36 +0900

    dev 23-06-02

--- app/src/main/java/com/logimanager/HomeActivity.java
+++ app/src/main/java/com/logimanager/HomeActivity.java
@@ -3,12 +3,18 @@ package com.logimanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+    Button btn_store,btn_release;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+    public void InitializeView()
+    {
+
     }
 }

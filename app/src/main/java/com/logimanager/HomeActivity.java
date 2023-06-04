cbcddbbeb9cb69dce3bc583617cd4e35f7c35ab2
JeongHun <JeongHun.Seo@outlook.kr>
2023-06-04 20:48:42 +0900

    update codes 23-06-04

--- app/src/main/java/com/logimanager/HomeActivity.java
+++ app/src/main/java/com/logimanager/HomeActivity.java
@@ -4,23 +4,22 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeActivity extends AppCompatActivity {
-    private SQLiteDatabase db;
-    private SimpleCursorAdapter adapter;
-    ListView db_table;
     ItemDBHelper itemDBHelper = new ItemDBHelper(this);
+    ListView db_table;
     Button btn_store;
     Button btn_release;
     private final ActivityResultLauncher<ScanOptions> store_scan_qr = registerForActivityResult(new ScanContract(),
@@ -28,8 +27,16 @@ public class HomeActivity extends AppCompatActivity {
                 if(result.getContents() == null) {
                     Toast.makeText(HomeActivity.this, "취소", Toast.LENGTH_LONG).show();
                 } else {
-                    Toast.makeText(HomeActivity.this,  result.getContents()+" 입고", Toast.LENGTH_LONG).show();
-                    itemDBHelper.insertProduct(result.getContents(),(1));
+                    String data1 = result.getContents().toString();
+                    Toast.makeText(HomeActivity.this,  data1+" 입고", Toast.LENGTH_LONG).show();
+                    try {
+                        itemDBHelper.increaseProductCount(data1);
+                        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                        startActivity(intent);
+                    }
+                    catch (Exception e){
+                        Toast.makeText(HomeActivity.this,  e.toString(), Toast.LENGTH_LONG).show();
+                    }
                 }
             });
     private final ActivityResultLauncher<ScanOptions> release_scan_qr = registerForActivityResult(new ScanContract(),
@@ -37,10 +44,47 @@ public class HomeActivity extends AppCompatActivity {
                 if(result.getContents() == null) {
                     Toast.makeText(HomeActivity.this, "취소", Toast.LENGTH_LONG).show();
                 } else {
-                    Toast.makeText(HomeActivity.this,  result.getContents()+" 출고", Toast.LENGTH_LONG).show();
-                    itemDBHelper.decreaseProductCount(result.getContents());
+                    String data2 = result.getContents().toString();
+                    Toast.makeText(HomeActivity.this,  data2+" 입고", Toast.LENGTH_LONG).show();
+                    try {
+                        itemDBHelper.decreaseProductCount(data2);
+                        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                        startActivity(intent);
+                    }
+                    catch (Exception e){
+                        Toast.makeText(HomeActivity.this,  e.toString(), Toast.LENGTH_LONG).show();
+                    }
                 }
             });
+
+    public void getListViewOnDB(){
+        List<String> itemList = new ArrayList<>();
+        List<Integer> itemCountList = new ArrayList<>();
+        List<Integer> imageList = new ArrayList<>();
+        int num1 = itemDBHelper.getCountForItem("adidas");
+        int num2 = itemDBHelper.getCountForItem("nike");
+        int num3 = itemDBHelper.getCountForItem("vans");
+        int num4 = itemDBHelper.getCountForItem("converse");
+        int num5 = itemDBHelper.getCountForItem("asics");
+        itemList.add("adidas");
+        itemList.add("nike");
+        itemList.add("vans");
+        itemList.add("converse");
+        itemList.add("asics");
+        itemCountList.add(num1);
+        itemCountList.add(num2);
+        itemCountList.add(num3);
+        itemCountList.add(num4);
+        itemCountList.add(num5);
+        imageList.add(R.drawable.adidas);
+        imageList.add(R.drawable.nike);
+        imageList.add(R.drawable.vans);
+        imageList.add(R.drawable.converse);
+        imageList.add(R.drawable.asics);
+
+        CustomAdapter adapter = new CustomAdapter(this, itemList, itemCountList, imageList);
+        db_table.setAdapter(adapter);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,19 +94,6 @@ public class HomeActivity extends AppCompatActivity {
         btn_store = findViewById(R.id.btn_store);
         btn_release = findViewById(R.id.btn_release);
         db_table = findViewById(R.id.db_table);
-
-        db = itemDBHelper.getReadableDatabase();
-        Cursor cursor = db.rawQuery("SELECT id, count FROM products", null);
-        String[] fromColumns = { "id", "count" };
-
-        // View ID 배열
-        int[] toViews = { android.R.id.text1, android.R.id.text2 };
-
-        // 어댑터 생성
-        adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, cursor, fromColumns, toViews, 0);
-
-        // ListView에 어댑터 설정
-        db_table.setAdapter(adapter);
 
         btn_store.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -76,22 +107,6 @@ public class HomeActivity extends AppCompatActivity {
                 release_scan_qr.launch(new ScanOptions());
             }
         });
-
-
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        // 사용한 리소스 정리
-        if (adapter != null) {
-            adapter.swapCursor(null);
-        }
-        if (db != null) {
-            db.close();
-        }
-        if (itemDBHelper != null) {
-            itemDBHelper.close();
-        }
+        getListViewOnDB();
     }
 }
--- app/src/main/java/com/logimanager/ItemDBHelper.java
+++ app/src/main/java/com/logimanager/ItemDBHelper.java
@@ -1,12 +1,15 @@
 package com.logimanager;
+
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class ItemDBHelper extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "products.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "product.db";
+    private static final int DATABASE_VERSION = 1; // 변경된 버전
 
     public ItemDBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -15,8 +18,9 @@ public class ItemDBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         // Create the table
-        String createTableQuery = "CREATE TABLE products (" +
-                "id TEXT PRIMARY KEY," +
+        String createTableQuery = "CREATE TABLE IF NOT EXISTS product (" +
+                "_id INTEGER PRIMARY KEY," +
+                "id TEXT," +
                 "count INTEGER" +
                 ")";
         db.execSQL(createTableQuery);
@@ -24,26 +28,38 @@ public class ItemDBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        // Handle database upgrades if needed
+
     }
 
-    public void insertProduct(String id, int count) {
-        SQLiteDatabase db = this.getWritableDatabase();
+    @SuppressLint("Range")
+    public int getCountForItem(String id){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] projection = {"count"};
+        String selection = "id=?";
+        String[] selectionArgs = {id};
 
-        ContentValues values = new ContentValues();
-        values.put("id", id);
-        values.put("count", count);
+        Cursor cursor = db.query("product", projection, selection, selectionArgs, null, null, null);
 
-        db.insert("products", null, values);
-        db.close();
+        int count = 0;
+        if (cursor != null && cursor.moveToFirst()) {
+            count = cursor.getInt(cursor.getColumnIndex("count"));
+            cursor.close();
+        }
+
+        return count;
     }
 
     public void decreaseProductCount(String id) {
         SQLiteDatabase db = this.getWritableDatabase();
 
-        String updateQuery = "UPDATE products SET count = count - 1 WHERE id = ?";
+        String updateQuery = "UPDATE product SET count = count - 1 WHERE id = ?";
         db.execSQL(updateQuery, new String[]{id});
+    }
 
-        db.close();
+    public void increaseProductCount(String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        String updateQuery = "UPDATE product SET count = count + 1 WHERE id = ?";
+        db.execSQL(updateQuery, new String[]{id});
     }
 }
